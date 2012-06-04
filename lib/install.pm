71f267d0a91b618d1058a24c3eeccd2ed354d01a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-06-04 15:01:22 -0500

    added dicty-pack task for system packages installation

--- lib/install.pm
+++ lib/install.pm
@@ -1,31 +1,39 @@
 package  install;
 use strict;
 use Rex -base;
-use Rex::Commands::Run; 
+use Rex::Commands::Run;
 use Rex::Commands::Gather;
+use Rex::Commands::Pkg;
 
-desc 'install a package(--package) (only redhat and ubuntu supported) with sudo access';
-task 'package' => sub {
-	my ($param) = @_;
-	die "no package name given\n" if not defiend $param->{package};
-	$package = $param->{package};
-
-	my $cmd;
-	# -- guess the os for command
-	if (is_redhat) {
-		$cmd = "sudo yum -y install $package";
-	}
-	elsif (is_debian) {
-		$cmd = "sudo apt-get -y install $package";
-	}
-	else {
-		die "your Os is not supported\n";
-	}
-	run $cmd;
+#desc 'install a package(--package) (only redhat and ubuntu supported) with sudo access';
+#task 'package' => sub {
+#	my ($param) = @_;
+#	die "no package name given\n" if not defiend $param->{package};
+#	$package = $param->{package};
+#
+#	my $cmd;
+#	 -- guess the os for command
+#	if (is_redhat) {
+#		$cmd = "sudo yum -y install $package";
+#	}
+#	elsif (is_debian) {
+#		$cmd = "sudo apt-get -y install $package";
+#	}
+#	else {
+#		die "your Os is not supported\n";
+#	}
+#	run $cmd;
+#};
+#
+desc
+    'install system packages needed for deployment of dictybase web applications';
+task 'dicty-pack' => sub {
+    update_package_db;
+    install package => [
+        qw/gcc curl wget make man gd db4 db4-devel git vim-enhanced httpd
+            mod_fastcgi htop acl/
+    ];
 };
-
-
-
 
 1;    # Magic true value required at end of module
 
