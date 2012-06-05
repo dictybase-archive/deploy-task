7c8c1b9ab5bb163f68e73522403e3ee731338206
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-06-05 15:10:59 -0500

    add system option for perlbrew install

--- lib/perlbrew.pm
+++ lib/perlbrew.pm
@@ -10,7 +10,8 @@ use Rex::Commands::Run;
 # Module implementation
 #
 
-desc 'install perlbrew (--install-root=[full-path-of-remote-folder] optional)';
+desc
+    'install perlbrew (--install-root=[full-path-of-remote-folder] optional,  --system=1) ';
 task 'install' => sub {
     my ($param) = @_;
     my $prepend
@@ -33,58 +34,66 @@ task 'install' => sub {
         );
     }
     else {
-            die
-                "need to install either of**curl* or ** wget**in remote machine\n";
+        die
+            "need to install either of**curl* or ** wget**in remote machine\n";
     }
 
     # source perlbrew
     my $root = $param->{'install-root'} || '~/perl5/perlbrew';
-    $root .= '/etc/bashrc';
-    run "echo \'source $root \' >> ~/.bashrc";
+
+    if ( exists $param->{system} ) {
+        sudo "echo export PERLBREW_ROOT=$root >> /etc/profile.d/perlbrew.sh";
+        sudo
+            'echo source ${PERLBREW_ROOT}/etc/bashrc >> ~/etc/profile.d/perlbrew.sh';
+    }
+    else {
+        run "echo export PERLBREW_ROOT=$root >> ~/.bashrc";
+        run 'echo source ${PERLBREW_ROOT}/etc/bashrc >> ~/.bashrc';
+    }
 
     # source in current shell
-    run 'source ~/.bashrc';
+    run 'source /etc/profile && source ~/.bash_profile';
     needs perlbrew 'check';
     print "perlbrew is installed\n";
- };
+};
 
 desc 'check if perlbrew is installed';
 task 'check' => sub {
-		if ( run 'echo ${PERLBREW_ROOT}' ) {
-        }
-        else {
-            warn "perlbrew is not installed or not being set properly\n";
-            die "install perlbrew using the install-perlbrew task\n";
-        }
+    if ( run 'echo ${PERLBREW_ROOT}' ) {
+    }
+    else {
+        warn "perlbrew is not installed or not being set properly\n";
+        die "install perlbrew using the install-perlbrew task\n";
+    }
 };
 
-desc 'upgrade perlbrew'; 
+desc 'upgrade perlbrew';
 task 'upgrade' => sub {
     needs perlbrew 'check';
-	say run '$PERLBREW_ROOT/bin/perlbrew self-upgrade';
+    say run '$PERLBREW_ROOT/bin/perlbrew self-upgrade';
 };
 
 desc 'list available perl';
 task 'list' => sub {
     needs perlbrew 'check';
-    say run '$PERLBREW_ROOT/bin/perlbrew list'; 
+    say run '$PERLBREW_ROOT/bin/perlbrew list';
 };
 
 desc 'list of installable perl';
 task 'available' => sub {
     needs perlbrew 'check';
-    say run '$PERLBREW_ROOT/bin/perlbrew available'; 
+    say run '$PERLBREW_ROOT/bin/perlbrew available';
 };
 
 desc 'make this the default perl';
 task 'switch' => sub {
-    	needs perlbrew 'check';
+    needs perlbrew 'check';
 
-        my ($param) = @_;
-        my $version = $param->{'perl-version'} or die "pass a version argument with --perl-version=\n";
-        say run "\$PERLBREW_ROOT/bin/perlbrew switch $version";
+    my ($param) = @_;
+    my $version = $param->{'perl-version'}
+        or die "pass a version argument with --perl-version=\n";
+    say run "\$PERLBREW_ROOT/bin/perlbrew switch $version";
 };
-
 
 1;    # Magic true value required at end of module
 
