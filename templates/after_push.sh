681bab8dbc5ecee8e235fb15e96120a7bd16bd3b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-06-12 13:00:32 -0500

    refactored deployment into smaller phases

--- templates/after_push.sh
+++ templates/after_push.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # $1 = root of the web application folder
-installer=$1-dependencies
-post_install=$1-daemontool
+deployer=$1
 APP_DIR=$2
 PROJECT=`basename $APP_DIR`
 PERL_VERSION=$3
@@ -10,59 +9,82 @@ LOCAL_LIB=$PERL_VERSION\@$PROJECT
 SERVICE=$APP_DIR/service
 APP_SERVICE=$SERVICE/${PROJECT}runner
 RUN_FILE=$APP_SERVICE/run
-ENCRYPT_CONFIG_FOLDER=$4
-cpanm=$PERLBREW_ROOT/bin/cpanm
-perlbrew=$PERLBREW_ROOT/bin/perlbrew
+CONFIG_FOLDER=$4
 
-setup_daemontool() {
-	! [ -d $APP_SERVICE ] && mkdir -p $APP_SERVICE
-	if ! [ -e $RUN_FILE ];then 
-		cd $APP_DIR
-		deploy/$post_install $PROJECT $RUN_FILE $LOCAL_LIB $APP_DIR
 
-		chmod 1755 $APP_SERVICE
-		chmod 755 $RUN_FILE
-		ln -s $APP_SERVICE /service/$PROJECT
-	fi
-
-}
-
-copy_encrypted_config() {
-	if ! [ -z "$MOJO_MODE" ]; then
+copy_config() {
+	if [ -z "$MOJO_MODE" ]; then
 	    MOJO_MODE='production'
 	fi
 
-	actual_config=${ENC_CONFIG_FOLDER}/${PROJECT}/${MOJO_MODE}.yml
-	sample_config=${APP_DIR}/config/sample.yml
+	local config_folder=$1
+	local project=$2
+	local app_dir=$3
+
+	actual_config=${config_folder}/${project}/${MOJO_MODE}.yml
+	sample_config=${app_dir}/conf/sample.yml
 
      
-  if [ -e "$sample_config" ] && [ -e "$actual_config" ]; then
+  if [ -f "$actual_config" ] && [ -f "$sample_config" ]; then
         running_perl=`which perl`
-        $running_perl ${APP_DIR}/deploy/merge_config.pl $actual_config $sample_config $MOJO_MODE
+        $running_perl ${app_dir}/deploy/merge_config.pl $actual_config $sample_config $MOJO_MODE
   else
-        echo cannot find the config file $actual_config and $sample_config
+        echo cannot find the config files $actual_config and $sample_config
   fi
 }
 
-if [ -e $cpanm ]; then
-	echo "------> Upgrading cpanm"
-	$cpanm --self-upgrade
-else
-  echo "------> Installing cpanm"
-  $perlbrew install-cpanm
-fi
+setup_cpanm_perlbrew () {
 
-if ! [ `$perlbrew list | grep $PROJECT` ]; then
-	$perlbrew lib create $LOCAL_LIB
-fi
+  local cpanm=$PERLBREW_ROOT/bin/cpanm
+  local perlbrew=$PERLBREW_ROOT/bin/perlbrew
 
-echo "-------> using lib $LOCAL_LIB"
-export PERLBREW_ROOT=$PERLBREW_ROOT
-export PERLBREW_HOME=$PERLBREW_HOME
-source $PERLBREW_ROOT/etc/bashrc
-perlbrew use $LOCAL_LIB
+  if [ -e $cpanm ]; then
+	  echo "------> Upgrading cpanm"
+	  $cpanm --self-upgrade
+  else
+    echo "------> Installing cpanm"
+    $perlbrew install-cpanm
+  fi
+
+  if ! [ `$perlbrew list | grep $PROJECT` ]; then
+	  $perlbrew lib create $LOCAL_LIB
+  fi
+
+  echo "-------> using lib $LOCAL_LIB"
+  export PERLBREW_ROOT=$PERLBREW_ROOT
+  export PERLBREW_HOME=$PERLBREW_HOME
+  source ${PERLBREW_ROOT}/etc/bashrc
+
+  # install carton
+  cpanm -n Carton
+  perlbrew use $LOCAL_LIB
+}
+
+# There are few steps for deployment
+# 1. setup perlbrew and  cpanm environment
+# 2. install dependencies
+# 3. copy config file
+# 4. put the web application under the control of daemontools(optional)
+
 
 cd $APP_DIR
-if [ -x deploy/$installer ]; then
-  deploy/$installer cpanm $APP_DIR  && copy_encrypted_config && setup_daemontool
+if [ -x deploy/$deployer ]; then
+  setup_cpanm_perlbrew
+	source deploy/${deployer}
+
+	# handle dependencies using two hooks
+	before_install_dependencies $APP_DIR
+	after_install_dependencies $APP_DIR
+	install_dependencies $APP_DIR
+
+	# config file
+	copy_config $CONFIG_FOLDER $PROJECT $APP_DIR
+
+	# handle daemontools setup using two hooks
+	before_create_deamontools_runfile  $PROJECT $RUN_FILE $LOCAL_LIB $APP_DIR
+	create_deamontools_runfile  $PROJECT $RUN_FILE $LOCAL_LIB $APP_DIR
+	after_create_daemontools_runfile  $PROJECT $RUN_FILE $LOCAL_LIB $APP_DIR
+
+else
+  echo deployer script $deployer do not exist!!!!!
 fi
