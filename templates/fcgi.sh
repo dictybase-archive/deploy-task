451fa983e7292eb05d5d4346ffec71ff54e58f95
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-06-12 11:10:55 -0500

    added functions for creating daemontools service

--- templates/fcgi.sh
+++ templates/fcgi.sh
@@ -18,3 +18,44 @@ after_install_dependencies() {
 before_install_dependencies() {
   return
 }
+
+create_daemontool_runfile(){
+
+  local PROJECT=$1
+  local RUN_FILE=$2
+  local LOCAL_LIB=$3
+  local APP_DIR=$4
+
+  echo -e "#!/bin/sh\nexec 2>1&\n" > $RUN_FILE
+  echo "export HOME=$HOME" >> $RUN_FILE
+  echo -e "cd $APP_DIR\n" >> $RUN_FILE
+  echo "source ${PERLBREW_ROOT}/etc/bashrc" >> $RUN_FILE
+  echo "perlbrew use $LOCAL_LIB" >> $RUN_FILE
+  echo  "export MOJO_MODE=production" >> $RUN_FILE
+  echo  "exec setuidgid $USER carton exec -Ilib  -- plackup -E production -r -R template -s FCGI --nproc 4 -l /tmp/${PROJECT}.sock script/$PROJECT" >> $RUN_FILE
+
+}
+
+before_create_daemontool_runfile() {
+   return
+}
+
+after_create_daemontool_runfile() {
+
+  local PROJECT=$1
+  local RUN_FILE=$2
+  local LOCAL_LIB=$3
+  local APP_DIR=$4
+  local SERVICE=${APP_DIR}/service
+  local APP_SERVICE=${SERVICE}/${PROJECT}runner
+  local RUN_FILE=${APP_SERVICE}/run
+
+	! [ -d $APP_SERVICE ] && mkdir -p $APP_SERVICE
+
+	if  [ ! -e $RUN_FILE ];then 
+		cd $APP_DIR
+		chmod 1755 $APP_SERVICE
+		chmod 755 $RUN_FILE
+		ln -s $APP_SERVICE /service/${PROJECT}
+	fi
+}
